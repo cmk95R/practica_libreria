f147f923fddbbaecccbd4b3d4732d4291bd29ed6
andrés <olivera.h.andres@gmail.com>
2023-06-10 14:17:46 -0300

    Agrega función editar

--- productos/index.js
+++ productos/index.js
@@ -1,10 +1,10 @@
-/* const {leerJSON,escribirJSON} = require('../data');
-const Producto = require('./Producto'); */
+const {leerJSON,escribirJSON} = require('../data');
+const Producto = require('./Producto'); 
 
 module.exports = {
  //productos: leerJSON()
- listar: require('./listar')
+ listar: require('./listar'),
  //agregar: require ('./')
- //editar: require ('./')
+ editar: require('./editar')
  //buscar: require ('./')
 };
